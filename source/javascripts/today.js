75d8369635510cdba87ed9252c79015a03def1e2
Joshua Steele <joshukraine@gmail.com>
2016-04-05 12:40:12 +0300

    Remove bourbon, et al. Better to install fresh when starting new site.

--- source/javascripts/today.js
+++
@@ -1,7 +0,0 @@
-var $ = require('jquery');
-var moment = require('moment')
-
-$(function() {
-  var date = moment().format('dddd, MMMM Do, YYYY');
-  $('#today').html('<p>Today is ' + date + '.</p>');
-});
