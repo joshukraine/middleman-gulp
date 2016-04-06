efe06f6e70cb3c40696c74b5278e2d3d697be813
Joshua Steele <joshukraine@gmail.com>
2016-04-06 11:33:40 +0300

    Add jQuery and Moment.js

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,2 @@
 require('_test');
+require('today');
---
+++ source/javascripts/today.js
@@ -0,0 +1,7 @@
+var $ = require('jquery');
+var moment = require('moment')
+
+$(function() {
+  var date = moment().format('dddd, MMMM Do, YYYY');
+  $('.today').html('<p>Hello from jQuery land! Today is ' + date + '.</p>');
+});
