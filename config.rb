2cfb85ca4aaf09d00a3de43222d22233fbd403a9
Joshua Steele <joshukraine@gmail.com>
2016-04-08 13:33:29 +0300

    Add commented configs

--- config.rb
+++ config.rb
@@ -11,26 +11,37 @@ page "/*.json", layout: false
 page "/*.txt", layout: false
 page "/404.html", directory_index: false
 
-configure :development do
-  # activate :livereload
-end
-
-activate :external_pipeline,
-  name: :gulp,
-  command: build? ? "npm run production" : "npm run gulp",
-  source: ".tmp",
-  latency: 1
-
 configure :build do
   activate :minify_html do |html|
     html.remove_quotes = false
     html.remove_intertag_spaces = true
   end
 
-  # Ignore the CSS file Middleman normally generates
-  # Middleman expects `site.css.scss` → `site.css`
-  # We strip the `.css` to prevent Gulp generating `site.css.css`
-  # Add your site's main `.scss` filename (without the extension)
-  # To understand more, comment this out and run `middleman build`
+  # activate :asset_hash
+  # activate :asset_host,
+  #   host: "//cdn.cloudfront.net",
+  #   rewrite_ignore: ["*.js"]
+
+  # set :url_root, "http://example.com"
+
+  # https://github.com/Aupajo/middleman-search_engine_sitemap
+  # activate :search_engine_sitemap
+
   ignore "stylesheets/site"
 end
+
+# https://github.com/fredjean/middleman-s3_sync
+# activate :s3_sync do |s3|
+#   s3.bucket = "your-bucket-name"
+#   s3.aws_access_key_id = ENV["AWS_ACCESS_KEY_ID"]
+#   s3.aws_secret_access_key = ENV["AWS_SECRET_ACCESS_KEY"]
+#   s3.prefer_gzip = true
+#   s3.index_document = "index.html"
+#   s3.error_document = "404.html"
+# end
+
+activate :external_pipeline,
+  name: :gulp,
+  command: build? ? "npm run production" : "npm run gulp",
+  source: ".tmp",
+  latency: 1
