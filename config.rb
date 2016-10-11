6afeccc03169bdd0734580fc403608b33fc7cff7
Joshua Steele <joshukraine@gmail.com>
2016-10-11 15:32:35 +0300

    Add pry activation to environments/console.rb

--- config.rb
+++ config.rb
@@ -11,10 +11,6 @@ page "/*.json", layout: false
 page "/*.txt", layout: false
 page "/404.html", directory_index: false
 
-# configure :development do
-#   activate :pry
-# end
-
 configure :build do
   activate :minify_html do |html|
     html.remove_quotes = false
