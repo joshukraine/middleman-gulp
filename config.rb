6511888ea3b502aa500b72dbc0a042ce54da99a3
Joshua Steele <joshukraine@gmail.com>
2016-10-11 20:24:16 +0300

    Move build block back to production.rb.

--- config.rb
+++ config.rb
@@ -8,34 +8,6 @@ page "/*.json", layout: false
 page "/*.txt", layout: false
 page "/404.html", directory_index: false
 
-configure :build do
-  activate :external_pipeline,
-    name: :gulp,
-    command: "npm run production",
-    source: ".tmp",
-    latency: 1
-
-  activate :gzip
-
-  # activate :asset_hash
-  # activate :asset_host,
-  #   host: "//cdn.cloudfront.net",
-  #   rewrite_ignore: ["*.js"]
-
-  # set :url_root, "http://example.com"
-
-  # https://github.com/Aupajo/middleman-search_engine_sitemap
-  # activate :search_engine_sitemap
-
-  ignore "javascripts/all.js"
-  ignore "stylesheets/site"
-
-  activate :minify_html do |html|
-    html.remove_quotes = false
-    html.remove_intertag_spaces = true
-  end
-end
-
 # https://github.com/fredjean/middleman-s3_sync
 # activate :s3_sync do |s3|
 #   s3.bucket = "your-bucket-name"
