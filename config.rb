ca0b4413fb6f4a2ae63b09ee24fcb6fce907eeb3
Joshua Steele <joshukraine@gmail.com>
2016-10-11 15:36:30 +0300

    :zap: Clean unneeded directory settings.

--- config.rb
+++ config.rb
@@ -1,8 +1,5 @@
 activate :directory_indexes
 
-# set :css_dir, "stylesheets"
-# set :js_dir, "javascripts"
-# set :images_dir, "images"
 set :relative_links, true
 set :haml, { ugly: true, format: :html5 }
 
