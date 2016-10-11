05c6ffd14f0e12602462a53f946b00c11fcbacc7
Joshua Steele <joshukraine@gmail.com>
2016-10-11 20:35:11 +0300

    :tired_face: Move build block back to config.rb.
    * On local machine, middle build see's the production.rb file just fine.
    * On travis, it doesn't

--- environments/production.rb
+++ environments/production.rb
@@ -1,29 +1,2 @@
 # https://middlemanapp.com/basics/upgrade-v4/#environments-and-changes-to-configure-blocks
 
-configure :build do
-  activate :external_pipeline,
-    name: :gulp,
-    command: "npm run production",
-    source: ".tmp",
-    latency: 1
-
-  activate :gzip
-
-  # activate :asset_hash
-  # activate :asset_host,
-  #   host: "//cdn.cloudfront.net",
-  #   rewrite_ignore: ["*.js"]
-
-  # set :url_root, "http://example.com"
-
-  # https://github.com/Aupajo/middleman-search_engine_sitemap
-  # activate :search_engine_sitemap
-
-  ignore "javascripts/all.js"
-  ignore "stylesheets/site"
-
-  activate :minify_html do |html|
-    html.remove_quotes = false
-    html.remove_intertag_spaces = true
-  end
-end
