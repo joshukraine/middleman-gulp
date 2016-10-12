39a8b10fdfba477378e8220d9b67f877cdfb3e8b
Joshua Steele <joshukraine@gmail.com>
2016-10-12 13:55:56 +0300

    Add test and production environment files for completeness.

---
+++ environments/production.rb
@@ -0,0 +1,4 @@
+# This file is for reference only. Actual settings for the production
+# environment are stored in the build configuation block in config.rb.
+#
+# https://middlemanapp.com/basics/upgrade-v4/#environments-and-changes-to-configure-blocks
