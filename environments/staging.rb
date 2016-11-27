5acd277a5dd5bf3c12ff4b1d0ea98f13e2983adc
Joshua Steele <joshukraine@gmail.com>
2016-11-27 13:46:55 +0200

    :sparkles: Implement environment-specific deploys.

--- environments/staging.rb
+++ environments/staging.rb
@@ -1,18 +1,9 @@
-# Use this file for production-specific deployment configs.
+# Use this file for staging-specific deployment configs.
 # https://middlemanapp.com/basics/upgrade-v4/#environments-and-changes-to-configure-blocks
-
-# activate :asset_hash
-# activate :asset_host,
-#   host: "//<your-cloudfront-domain>.cloudfront.net"
-
-# set :url_root, "http://example.com"
-
-# https://github.com/Aupajo/middleman-search_engine_sitemap
-# activate :search_engine_sitemap
 
 # https://github.com/fredjean/middleman-s3_sync
 # activate :s3_sync do |s3|
-#   s3.bucket = "example.com"
+#   s3.bucket = "staging.example.com"
 #   s3.aws_access_key_id = ENV["AWS_ACCESS_KEY_ID"]
 #   s3.aws_secret_access_key = ENV["AWS_SECRET_ACCESS_KEY"]
 #   s3.prefer_gzip = true
