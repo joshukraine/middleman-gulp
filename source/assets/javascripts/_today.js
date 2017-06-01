bc37f07e53ccf4c8bc0b141ed6504bfdadc4a7c3
Joshua Steele <joshukraine@gmail.com>
2017-06-01 12:53:37 +0300

    Reference Moment.js on home page.

--- source/assets/javascripts/_today.js
+++ source/assets/javascripts/_today.js
@@ -3,5 +3,5 @@ var moment = require('moment');
 
 $(function() {
   var date = moment().format('dddd, MMMM Do, YYYY');
-  $('.today').html('<p style="color: blue">Hello from jQuery land! Today is ' + date + '.</p>');
+  $('.today').html('<p style="color: blue">Hello from jQuery land!<br/>Moment.js says today is ' + date + '.</p>');
 });
